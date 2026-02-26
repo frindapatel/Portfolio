f8b857edc8bc11add13c2a359510a07be15d5b7e
frindapatel <patelfrinda@gmail.com>
2026-02-26 13:12:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const text = ["BScIT Student", "Aspiring Web Developer", "Tech Enthusiast"];
+let index = 0;
+let charIndex = 0;
+const typingElement = document.querySelector(".typing");
+
+function typeEffect() {
+    if (charIndex < text[index].length) {
+        typingElement.textContent += text[index].charAt(charIndex);
+        charIndex++;
+        setTimeout(typeEffect, 100);
+    } else {
+        setTimeout(eraseEffect, 2000);
+    }
+}
+
+function eraseEffect() {
+    if (charIndex > 0) {
+        typingElement.textContent = text[index].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(eraseEffect, 60);
+    } else {
+        index = (index + 1) % text.length;
+        setTimeout(typeEffect, 500);
+    }
+}
+
+typeEffect();
